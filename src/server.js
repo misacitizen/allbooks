8208198f53a512006ac426c604991a2f65dbd17b
Misael Santos <misainvest1000@gmail.com>
2025-04-14 22:39:15 -0300

    Altera o server

--- src/server.js
+++ src/server.js
@@ -18,6 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  consolo.log ("Boas-vindas a API do AllBooks")
-  console.log("API disponível através da URL em http://localhost:8000")
+  console.log("API disponivel em http://localhost:8000")
 })
